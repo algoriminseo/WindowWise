adab844c231d0e390f01342517c6d5cce42e8984
minseokim <minseohappy07@gmail.com>
2026-07-01 15:29:04 -0500

    feat:Bassic UI for WindowWIse

---
+++ src/WindowWise.App/App.xaml.cs
@@ -0,0 +1,7 @@
+using System.Windows;
+
+namespace WindowWise;
+
+public partial class App : Application
+{
+}
---
+++ src/WindowWise.App/Views/MainWindow.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows;
+
+namespace WindowWise.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
