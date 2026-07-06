b88eb365d93d7b9fd16b5c6e1eeedfec0e709532
minseokim <minseohappy07@gmail.com>
2026-07-06 12:47:09 -0500

    feat: connected ClipboardService to MainWindow

--- src/WindowWise.App/Views/MainWindow.xaml.cs
+++ src/WindowWise.App/Views/MainWindow.xaml.cs
@@ -1,10 +1,14 @@
 using System.Windows;
 using System.Windows.Controls;
+using WindowWise.Services;
 
 namespace WindowWise.Views;
 
 public partial class MainWindow : Window
 {
+    private readonly ClipboardHistoryService _clipboardHistoryService;
+    private readonly ClipboardMonitorService _clipboardMonitorService;
+
     public void ShowOverview()
     {
         MainContent.Content = new OverviewView();
@@ -14,6 +18,12 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+
+        _clipboardHistoryService = new ClipboardHistoryService();
+        _clipboardMonitorService = new ClipboardMonitorService(_clipboardHistoryService);
+
+        SourceInitialized += MainWindow_SourceInitialized;
+        Closed += MainWindow_Closed;
         ShowOverview();
     }
 
@@ -54,8 +64,24 @@ public partial class MainWindow : Window
         MainContent.Content = new AudioManagerView();
         SetActiveNavigation(AudioManagerButton);
     }
+
+
+    // Navigate to AudioManagerView
     public void AudioManagerButton_Click(object sender, RoutedEventArgs e)
     {
         ShowAudioManager();
     }
+
+    // Navigate to WindowLayoutsView
+    private void MainWindow_SourceInitialized(object? sender, EventArgs e)
+    {
+        _clipboardMonitorService.Start(this);
+    }
+    // close the clipboard monitor service when the window is closed
+    private void MainWindow_Closed(object? sender, EventArgs e)
+    {
+        _clipboardMonitorService.Dispose();
+    }
+
+
 }
