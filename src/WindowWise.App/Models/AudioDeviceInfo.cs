747ac7feebe843754d2f59223e499e8dca53c890
eagles-chiu-choi <choichiu96@gmail.com>
2026-07-16 23:04:18 +0900

    feat(audio): propagate device changes through PropertyChanged

--- src/WindowWise.App/Models/AudioDeviceInfo.cs
+++ src/WindowWise.App/Models/AudioDeviceInfo.cs
@@ -1,28 +1,49 @@
 using NAudio.CoreAudioApi;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
 using WindowWise.Services;
 namespace WindowWise.Models
 {
-    public sealed class AudioDeviceInfo : IDisposable
+    public sealed class AudioDeviceInfo : IDisposable, INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler? PropertyChanged;
         private readonly WindowsAudioDeviceService _AudioServiceNotifier;
-        public MMDevice? DefaultDevice { get; private set; }
-        public Dictionary<string, MMDevice> Devices { get; private set; } = new();
-        public AudioDeviceInfo(string id, string name, bool isActive)
+        public MMDevice? DefaultDevice {
+            get => _DefaultDevice;
+            private set
+            {
+                _DefaultDevice = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(DefaultDevice)));
+            }
+        }
+        private MMDevice? _DefaultDevice;
+        public Dictionary<string, MMDevice>? Devices {
+            get => _Devices;
+            private set
+            {
+                _Devices = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Devices)));
+            }
+        }
+        private Dictionary<string, MMDevice>? _Devices;
+        public AudioDeviceInfo()
         {
             _AudioServiceNotifier = new WindowsAudioDeviceService();
-            DefaultDevice = _AudioServiceNotifier.GetDefaultOutputDevice();
-            Devices = _AudioServiceNotifier.GetDevices();
+            _DefaultDevice = _AudioServiceNotifier.GetDefaultOutputDevice();
+            _Devices = _AudioServiceNotifier.GetDevices();
             _AudioServiceNotifier.DeviceChanged += Refresh;
         }
 
-        public void Refresh()
+        private void Refresh()
         {
             DefaultDevice = _AudioServiceNotifier.GetDefaultOutputDevice();
             Devices = _AudioServiceNotifier.GetDevices();
         }
+
+
+
 
         void IDisposable.Dispose() {
             _AudioServiceNotifier.DeviceChanged -= Refresh;
--- src/WindowWise.App/ViewModels/AudioManagerViewModel.cs
+++ src/WindowWise.App/ViewModels/AudioManagerViewModel.cs
@@ -1,15 +1,20 @@
 using NAudio.CoreAudioApi;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO.Packaging;
 using System.Text;
-using WindowWise.Services;
+using WindowWise.Models;
 namespace WindowWise.ViewModels
 {
     public sealed class AudioManagerViewModel
     {
+        public AudioDeviceInfo AudioInfo { get; } = new();
 
         public AudioManagerViewModel()
         {
+            
         }
     }
 }
