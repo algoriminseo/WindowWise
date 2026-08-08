8ec2e1c0d6b3cbb386d0083382f9e5679e4b9f90
eagles-chiu-choi <choichiu96@gmail.com>
2026-08-09 02:02:57 +0900

    feat(audio): connect the UI to the preset system

--- src/WindowWise.App/ViewModels/AudioManagerViewModel.cs
+++ src/WindowWise.App/ViewModels/AudioManagerViewModel.cs
@@ -6,15 +6,18 @@ using System.ComponentModel;
 using System.IO.Packaging;
 using System.Text;
 using WindowWise.Models;
+using WindowWise.Services;
 namespace WindowWise.ViewModels
 {
     public sealed class AudioManagerViewModel
     {
         public AudioDeviceInfo AudioInfo { get; } = new();
-
+        public AudioPreset AudioPreset { get; }
+        public AudioPresetInfo PresetInfo { get; }
         public AudioManagerViewModel()
         {
-            
+            AudioPreset = new AudioPreset(AudioInfo);
+            PresetInfo = new AudioPresetInfo(AudioPreset);
         }
     }
 }
